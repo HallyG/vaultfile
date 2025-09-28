2f141930b0436eaa4b24f21da0fd7489c383c05c
Hal Garland <28985585+HallyG@users.noreply.github.com>
2025-09-28 18:42:26 +0100

    Simplfy vaultfile format logic (#20)
    The Vaultfile format logic is responsible for marshalling and unmarshalling a vaultfile header from binary. Currently, the implementation is quite confusing. Lets fix this by implementing the standard interface encoding.BinaryMarshaler and separating functionality into different files (kdf, hmac, header, etc).
    
    * Improve format tests
    
    * Use encoding.BinaryMarshaler for KDFParams
    
    * Rename MAC to HMAC
    
    * Remove unused params
    
    * Split format into separate files
    
    * Improve format tests
    
    * Fix vault tests
    
    * Simplify HMAC logic
    
    * Add header validation tests
    
    * Add more test cases for different types of input for Vault
    
    * Dynamically fetch all go files for testing

---
+++ internal/vault/format/header.go
@@ -0,0 +1,122 @@
+package format
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/binary"
+	"fmt"
+)
+
+const (
+	MagicNumber           = "HGVF"
+	MagicNumberLen        = len(MagicNumber)
+	VersionLen            = 1
+	SaltLen               = 16
+	NonceLen              = 24
+	TotalPayloadLengthLen = 2
+	HMACLen               = sha256.Size
+	TotalHeaderLen        = MagicNumberLen + VersionLen + SaltLen + NonceLen + KDFParamsLen + TotalPayloadLengthLen + HMACLen
+)
+
+type Header struct {
+	MagicNumber        [MagicNumberLen]byte
+	Version            Version
+	Salt               [SaltLen]byte
+	Nonce              [NonceLen]byte
+	KDFParams          KDFParams
+	TotalPayloadLength uint16
+	HMAC               [HMACLen]byte
+}
+
+// Validate checks if the Header fields are valid.
+func (h *Header) Validate() error {
+	if !bytes.Equal(h.MagicNumber[:], []byte(MagicNumber)) {
+		return fmt.Errorf("magic number: expected %q, got %q", MagicNumber, h.MagicNumber)
+	}
+
+	if h.Version != VersionV1 {
+		return fmt.Errorf("version: expected %s, got %s", VersionV1, h.Version)
+	}
+
+	if h.TotalPayloadLength < uint16(TotalHeaderLen) {
+		return fmt.Errorf("total payload length must be at least %d bytes, got %d", TotalHeaderLen, h.TotalPayloadLength)
+	}
+
+	return nil
+}
+
+// MarshalBinary implements [encoding.BinaryMarshaler].
+func (h *Header) MarshalBinary() ([]byte, error) {
+	if err := h.Validate(); err != nil {
+		return nil, fmt.Errorf("invalid: %w", err)
+	}
+
+	buf := make([]byte, TotalHeaderLen)
+	offset := 0
+
+	copy(buf[offset:offset+MagicNumberLen], h.MagicNumber[:])
+	offset += MagicNumberLen
+
+	buf[offset] = byte(h.Version)
+	offset += VersionLen
+
+	copy(buf[offset:offset+SaltLen], h.Salt[:])
+	offset += SaltLen
+
+	copy(buf[offset:offset+NonceLen], h.Nonce[:])
+	offset += NonceLen
+
+	kdfParamsBytes, err := h.KDFParams.MarshalBinary()
+	if err != nil {
+		return nil, fmt.Errorf("marshal KDF params: %w", err)
+	}
+	copy(buf[offset:offset+KDFParamsLen], kdfParamsBytes)
+	offset += KDFParamsLen
+
+	binary.BigEndian.PutUint16(buf[offset:offset+TotalPayloadLengthLen], h.TotalPayloadLength)
+	offset += TotalPayloadLengthLen
+
+	copy(buf[offset:offset+HMACLen], h.HMAC[:])
+
+	return buf, nil
+}
+
+// UnmarshalBinary implements [encoding.BinaryMarshaler].
+func (h *Header) UnmarshalBinary(data []byte) error {
+	if len(data) != TotalHeaderLen {
+		return fmt.Errorf("invalid length: got %d, expected %d", len(data), TotalHeaderLen)
+	}
+
+	offset := 0
+
+	copy(h.MagicNumber[:], data[offset:offset+MagicNumberLen])
+	offset += MagicNumberLen
+
+	h.Version = Version(data[offset])
+	offset += VersionLen
+
+	copy(h.Salt[:], data[offset:offset+SaltLen])
+	offset += SaltLen
+
+	copy(h.Nonce[:], data[offset:offset+NonceLen])
+	offset += NonceLen
+
+	var rawKDFParams [KDFParamsLen]byte
+	copy(rawKDFParams[:], data[offset:offset+NonceLen])
+	offset += KDFParamsLen
+
+	if err := h.KDFParams.UnmarshalBinary(rawKDFParams[:]); err != nil {
+		return fmt.Errorf("unmarshal KDF params: %w", err)
+	}
+
+	h.TotalPayloadLength = binary.BigEndian.Uint16(data[offset : offset+TotalPayloadLengthLen])
+	offset += TotalPayloadLengthLen
+
+	copy(h.HMAC[:], data[offset:offset+HMACLen])
+
+	if err := h.Validate(); err != nil {
+		return fmt.Errorf("invalid: %w", err)
+	}
+
+	return nil
+}
