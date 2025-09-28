c88caf9b999a465fdbbc6acf3b7178dbd75b2cc5
Hal Garland <28985585+HallyG@users.noreply.github.com>
2025-09-28 18:23:24 +0100

    Improve header tests

--- internal/vault/format/header_test.go
+++ internal/vault/format/header_test.go
@@ -1,72 +1,73 @@
 package format_test
 
-/*
+import (
+	"testing"
 
-	t.Run("returns error when magic number is invalid", func(t *testing.T) {
-		t.Parallel()
+	"github.com/HallyG/vaultfile/internal/vault/format"
+	"github.com/stretchr/testify/require"
+)
 
-		data := make([]byte, format.TotalHeaderLen)
-		copy(data, []byte("XXXX"))
-		data[format.MagicNumberLen] = byte(format.VersionV1)
+func TestHeaderValidate(t *testing.T) {
+	t.Parallel()
 
-		header, reader, err := format.ParseHeader(bytes.NewReader(data))
-		require.Nil(t, header)
-		require.Nil(t, reader)
-		require.EqualError(t, err, `unmarshal header: invalid: magic number: expected "HGVF", got "XXXX"`)
-	})
+	tests := map[string]struct {
+		expectedErr string
+		header      format.Header
+	}{
+		"returns error when invalid magic number": {
+			expectedErr: `magic number: expected "HGVF", got "XXXX"`,
+			header: format.Header{
+				MagicNumber: [4]byte{'X', 'X', 'X', 'X'},
+			},
+		},
+		"returns error when invalid version": {
+			expectedErr: "version: expected v1, got v0",
+			header: format.Header{
+				MagicNumber: [4]byte{'H', 'G', 'V', 'F'},
+			},
+		},
+		"returns error when zero payload length": {
+			expectedErr: "total payload length must be at least 88 bytes, got 0",
+			header: format.Header{
+				MagicNumber: [4]byte{'H', 'G', 'V', 'F'},
+				Version:     format.VersionV1,
+			},
+		},
+		"returns error when invalid payload length": {
+			expectedErr: "total payload length must be at least 88 bytes, got 87",
+			header: format.Header{
+				MagicNumber:        [4]byte{'H', 'G', 'V', 'F'},
+				Version:            format.VersionV1,
+				TotalPayloadLength: uint16(format.TotalHeaderLen) - 1,
+			},
+		},
+		"valid header with no payload": {
+			header: format.Header{
+				MagicNumber:        [4]byte{'H', 'G', 'V', 'F'},
+				Version:            format.VersionV1,
+				TotalPayloadLength: uint16(format.TotalHeaderLen),
+			},
+		},
+		"valid header with payload": {
+			header: format.Header{
+				MagicNumber:        [4]byte{'H', 'G', 'V', 'F'},
+				Version:            format.VersionV1,
+				TotalPayloadLength: uint16(format.TotalHeaderLen) + 1,
+			},
+		},
+	}
 
-	t.Run("returns error when version is invalid", func(t *testing.T) {
-		t.Parallel()
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
 
-		data := make([]byte, format.TotalHeaderLen)
-		copy(data, []byte(format.MagicNumber))
-		data[format.MagicNumberLen] = 99
+			err := test.header.Validate()
 
-		header, reader, err := format.ParseHeader(bytes.NewReader(data))
-		require.Nil(t, header)
-		require.Nil(t, reader)
-		require.EqualError(t, err, "unmarshal header: invalid: version: expected v1, got v99")
-	})
-*/
-
-// func createValidHeaderData(tb testing.TB) []byte {
-// 	tb.Helper()
-
-// 	data := make([]byte, TotalHeaderLen)
-// 	offset := 0
-
-// 	copy(data[offset:], []byte(MagicNumber))
-// 	offset += MagicNumberLen
-
-// 	data[offset] = byte(VersionV1)
-// 	offset += VersionLen
-
-// 	salt := [16]byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}
-// 	copy(data[offset:], salt[:])
-// 	offset += SaltLen
-
-// 	nonce := [24]byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24}
-// 	copy(data[offset:], nonce[:])
-// 	offset += NonceLen
-
-// 	kdfParams := KDFParams{
-// 		MemoryKiB:     1,
-// 		NumIterations: 1,
-// 		NumThreads:    1,
-// 	}
-
-// 	kdfBytes, err := kdfParams.MarshalBinary()
-// 	require.NoError(tb, err)
-// 	copy(data[offset:], kdfBytes[:])
-// 	offset += KDFParamsLen
-
-// 	binary.BigEndian.PutUint16(data[offset:], uint16(TotalHeaderLen+100))
-// 	offset += TotalPayloadLengthLen
-
-// 	mac := hmac.New(sha256.New, []byte("test-key"))
-// 	mac.Write(data[:offset])
-// 	hmacSum := mac.Sum(nil)
-// 	copy(data[offset:], hmacSum)
-
-// 	return data
-// }
+			if test.expectedErr != "" {
+				require.EqualError(t, err, test.expectedErr)
+			} else {
+				require.NoError(t, err)
+			}
+		})
+	}
+}
